3899d5ffc09b0c73dc6eba2d9e0f148e2fe24573
indicator <indicator@pdsu.com>
2022-08-05 14:57:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Indicator
+ * @create 2022-08-05 14:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
